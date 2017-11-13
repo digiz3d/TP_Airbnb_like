f6dc048d17e7c4a64f57777478fd653879e72b8f
kekos91 <kekos91@free.fr>
2017-11-13 22:52:50 +0100

    start implementing booking

--- routes/book.js
+++ routes/book.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res) {
     });
 });
 
-//Return all booking for an appartment
+//Return all booking for an apartment
 router.get('/:id', function (req, res) {
     Apartment.findOne({ _id: req.params.id }, function (err, appart) {
         if (err) throw err;
@@ -24,32 +24,28 @@ router.get('/:id', function (req, res) {
             res.json({ success: false });
         }
     });
+});
 
-    /*
-    if (dbContext.getHouseById(req.params.id) != null) {
-        if (!dbContext.isHouseBooked(req.params.id, req.params.startDate, req.params.endDate)) {
-            dbContext.setHouseBooked(req.params.id, req.params.startDate, req.params.endDate)
-            res.send("Housing booked :)");
-            var transport = mailer.getMailer();
-            transport.sendMail(mailer.getMessage("pintilie.c.a@gmail.com", "House booked !", "Congrats, you just booked the house n°" + 
-            req.params.id + " from " + req.params.startDate + " to " + req.params.endDate), function(error, info){
-                if(error){
-                    res.status(500);
-                    res.send("rip : "+error);
-                }
-                else {
-                    res.send("mail send to "+req.params.to);
-                }
+//lets book an apartment
+router.post('/:id/:startDate/:endDate', function(req, res) {
+    Apartment.findOne({ _id: req.params.id }, function (err, appart) {
+        if (err) throw err;
+
+        if (appart) {
+            // TODO : ICI VWALA
+            Booking.find({
+                apartment: appart._id,
+                start: { $gt: new Date(2014,1,10) }
+            }, function(err, books) {
+                res.json(books);
             });
-        } else {
-            res.status(409);
-            res.send("Housing already booked at this date. Change the dates and try again.");
         }
-    } else {
-        res.status(404);
-        res.send("Housing not found");
-    }
-    */
+        else {
+            res.json({ success: false });
+        }
+    });
+
+    
 });
 
 module.exports = router;
